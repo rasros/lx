1f70bae7034bcec5427f1ba9477b441dc382a243
Rasmus Ros <rasmus.ros@theca.com>
2026-04-11 18:53:49 +0200

    feat: add support for .ods and .odp archive suffixes

--- pkg/lx/archive.go
+++ pkg/lx/archive.go
@@ -15,7 +15,7 @@ import (
 // multi-part extensions must appear before their shorter suffix (.tar.gz before .gz).
 var archiveSuffixes = []string{
 	// ZIP-based (including document formats that are ZIP archives)
-	".zip", ".jar", ".war", ".ear", ".odt",
+	".zip", ".jar", ".war", ".ear", ".odt", ".ods", ".odp",
 	// TAR with compression (multi-part first)
 	".tar.gz", ".tar.bz2", ".tar.xz", ".tar.zst",
 	".tar.br", ".tar.lz4", ".tar.sz", ".tar.s2",
