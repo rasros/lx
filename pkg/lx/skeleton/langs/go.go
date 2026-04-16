2f717e90a24d0173b83c8280928299070aa480d3
Rasmus Ros <rasmus.ros@theca.com>
2026-04-16 10:59:31 +0200

    refactor: add support for grouped type declarations in GoEmitTypeDecl function

--- pkg/lx/skeleton/langs/go.go
+++ pkg/lx/skeleton/langs/go.go
@@ -13,55 +13,76 @@ func GoFuncVisible(n *gotreesitter.Node, src []byte, lang *gotreesitter.Language
 }
 
 func GoEmitTypeDecl(out, src []byte, lines [][]byte, n *gotreesitter.Node, lang *gotreesitter.Language, functions bool) []byte {
+	grouped := goIsGroupedTypeDecl(n, lang)
+	groupMembers := make([]byte, 0)
+
 	for i := 0; i < n.ChildCount(); i++ {
 		spec := n.Child(i)
 		if spec.Type(lang) != "type_spec" {
 			continue
 		}
-		nameNode := spec.ChildByFieldName("name", lang)
-		if nameNode == nil || !isUppercase(nameNode.Text(src)) {
-			continue
+
+		if grouped {
+			groupMembers = goEmitTypeSpec(groupMembers, src, lines, spec, lang, functions)
+		} else {
+			out = goEmitTypeSpec(out, src, lines, spec, lang, functions)
 		}
-		typeNode := spec.ChildByFieldName("type", lang)
-		if typeNode == nil {
-			continue
-		}
-		switch typeNode.Type(lang) {
-		case "struct_type":
-			out = goEmitStruct(out, src, lines, n, typeNode, lang)
-		case "interface_type":
-			out = goEmitInterface(out, src, lines, n, typeNode, lang, functions)
-		default:
-			out = content.AppendLines(out, lines, int(n.StartPoint().Row), int(n.EndPoint().Row))
-		}
+	}
+
+	if grouped && len(groupMembers) > 0 {
+		out = content.AppendLine(out, lines, int(n.StartPoint().Row))
+		out = append(out, groupMembers...)
+		out = content.AppendLine(out, lines, int(n.EndPoint().Row))
 	}
 	return out
 }
 
-func goEmitStruct(out, src []byte, lines [][]byte, typeDeclNode, structNode *gotreesitter.Node, lang *gotreesitter.Language) []byte {
+func goEmitTypeSpec(out, src []byte, lines [][]byte, spec *gotreesitter.Node, lang *gotreesitter.Language, functions bool) []byte {
+	nameNode := spec.ChildByFieldName("name", lang)
+	if nameNode == nil || !isUppercase(nameNode.Text(src)) {
+		return out
+	}
+
+	typeNode := spec.ChildByFieldName("type", lang)
+	if typeNode == nil {
+		return out
+	}
+
+	switch typeNode.Type(lang) {
+	case "struct_type":
+		return goEmitStruct(out, src, lines, spec, typeNode, lang)
+	case "interface_type":
+		return goEmitInterface(out, src, lines, spec, typeNode, lang, functions)
+	default:
+		return content.AppendLines(out, lines, int(spec.StartPoint().Row), int(spec.EndPoint().Row))
+	}
+}
+
+func goEmitStruct(out, src []byte, lines [][]byte, specNode, structNode *gotreesitter.Node, lang *gotreesitter.Language) []byte {
 	fieldList := findChildByType(structNode, "field_declaration_list", lang)
 	if fieldList == nil {
-		return content.AppendLines(out, lines, int(typeDeclNode.StartPoint().Row), int(typeDeclNode.EndPoint().Row))
+		return content.AppendLines(out, lines, int(specNode.StartPoint().Row), int(specNode.EndPoint().Row))
 	}
-	headerStart := int(typeDeclNode.StartPoint().Row)
-	headerEnd := int(fieldList.StartPoint().Row)
-	out = content.AppendLines(out, lines, headerStart, headerEnd)
+	headerStart := int(specNode.StartPoint().Row)
+	out = content.AppendLine(out, lines, headerStart)
+
 	for i := 0; i < fieldList.ChildCount(); i++ {
 		child := fieldList.Child(i)
-		if child.Type(lang) == "field_declaration" && goFieldIsExported(child, src, lang) {
+		if child.Type(lang) == "field_declaration" && goFieldIsExported(child, src, lang) && int(child.StartPoint().Row) != headerStart {
 			out = content.AppendLines(out, lines, int(child.StartPoint().Row), int(child.EndPoint().Row))
 		}
 	}
-	if closingRow := int(fieldList.EndPoint().Row); closingRow > headerEnd {
+
+	if closingRow := int(fieldList.EndPoint().Row); closingRow > headerStart {
 		out = content.AppendLine(out, lines, closingRow)
 	}
 	return out
 }
 
-func goEmitInterface(out, src []byte, lines [][]byte, typeDeclNode, ifaceNode *gotreesitter.Node, lang *gotreesitter.Language, functions bool) []byte {
-	headerStart := int(typeDeclNode.StartPoint().Row)
-	headerEnd := int(ifaceNode.StartPoint().Row)
-	out = content.AppendLines(out, lines, headerStart, headerEnd)
+func goEmitInterface(out, src []byte, lines [][]byte, specNode, ifaceNode *gotreesitter.Node, lang *gotreesitter.Language, functions bool) []byte {
+	headerStart := int(specNode.StartPoint().Row)
+	out = content.AppendLine(out, lines, headerStart)
+
 	if functions {
 		for i := 0; i < ifaceNode.ChildCount(); i++ {
 			child := ifaceNode.Child(i)
@@ -70,15 +91,25 @@ func goEmitInterface(out, src []byte, lines [][]byte, typeDeclNode, ifaceNode *g
 				continue
 			}
 			nameNode := findChildByType(child, "field_identifier", lang)
-			if nameNode != nil && isUppercase(nameNode.Text(src)) {
+			if nameNode != nil && isUppercase(nameNode.Text(src)) && int(child.StartPoint().Row) != headerStart {
 				out = content.AppendLines(out, lines, int(child.StartPoint().Row), int(child.EndPoint().Row))
 			}
 		}
 	}
-	if closingRow := int(ifaceNode.EndPoint().Row); closingRow > headerEnd {
+
+	if closingRow := int(ifaceNode.EndPoint().Row); closingRow > headerStart {
 		out = content.AppendLine(out, lines, closingRow)
 	}
 	return out
+}
+
+func goIsGroupedTypeDecl(n *gotreesitter.Node, lang *gotreesitter.Language) bool {
+	for i := 0; i < n.ChildCount(); i++ {
+		if n.Child(i).Type(lang) == "(" {
+			return true
+		}
+	}
+	return false
 }
 
 func goFieldIsExported(field *gotreesitter.Node, src []byte, lang *gotreesitter.Language) bool {
