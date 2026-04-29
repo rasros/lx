44dbc340570e70530e9cedd0acb6f0dc71a0d938
Rasmus Ros <rasmus.ros@theca.com>
2026-04-29 13:40:55 +0200

    test: add golden tests for -P prompt library

---
+++ cmd/lx/golden_promptlib_test.go
@@ -0,0 +1,17 @@
+package main
+
+import "testing"
+
+func TestGoldenPromptLib(t *testing.T) {
+	dir := setupPromptLibFixture(t)
+	runTestGolden(t, dir, []goldenTestCase{
+		{name: "800_promptlib_nested", args: []string{"-P", "go/test", "main.go"}},
+		{name: "801_promptlib_basename", args: []string{"-P", "refactor", "main.go"}},
+		{name: "802_promptlib_extension_probe", args: []string{"-P", "plan", "main.go"}},
+		{name: "803_promptlib_stack", args: []string{"-P", "refactor", "-P", "go/test", "main.go"}},
+		{name: "804_promptlib_xml", args: []string{"--xml", "-P", "go/test", "main.go"}},
+		{name: "805_promptlib_path_relative", args: []string{"-P", "./prompts/refactor.md", "main.go"}},
+		{name: "806_promptlib_not_found", args: []string{"-P", "missing", "main.go"}},
+		{name: "807_promptlib_with_section", args: []string{"-P", "go/test", "-s", "Code", "main.go"}},
+	})
+}
